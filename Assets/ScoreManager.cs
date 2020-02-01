192c5afb058e01c857233c2ad0243e5159f6fda1
Christoffer Holmgård <holmgard@gmail.com>
2020-02-01 21:07:47 +0100

    Fixed translucent boxes, added an isometric camera as a small test

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+    
+    public ScoreCube[] player1ScoreBoxes;
+    public ScoreCube[] player2ScoreBoxes;
+
+    public float player1Score = 0;
+    public float player2Score = 0;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+        int player1Boxes = 0;
+        float player1RawScore = 0;
+
+        int player2Boxes = 0;
+        float player2RawScore = 0;
+        
+        foreach (ScoreCube box in player1ScoreBoxes)
+        {
+            player1Boxes++;
+            player1RawScore += box.scoreNormalized;
+            player1Score = player1RawScore / (float) player1Boxes;
+        }
+        
+        foreach (ScoreCube box in player2ScoreBoxes)
+        {
+            player2Boxes++;
+            player2RawScore += box.scoreNormalized;
+            player2Score = player2RawScore / (float) player2Boxes;
+        }
+    }
+}
