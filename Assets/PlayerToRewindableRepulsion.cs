4b2f763b9b4d3ec5b600fd3e9fb543e506264b54
Artjoms-Jakovenko <artjoms.jakovenko@gmail.com>
2020-02-01 13:06:05 +0100

    Added repulsion from collidables

---
+++ Assets/PlayerToRewindableRepulsion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerToRewindableRepulsion : MonoBehaviour
+{
+    Rigidbody playerRigidbody;
+    public RewindablesManager rewindablesManager;
+
+    private void Awake()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (rewindablesManager.IsRewindable(collision.gameObject.GetInstanceID()))
+        {
+            Vector3 averageContact = Vector3.zero;
+            foreach (var contact in collision.contacts)
+            {
+                averageContact += contact.point;
+            }
+            averageContact /= collision.contactCount;
+            averageContact.y = 0.0F;
+
+            Vector3 repulsiveForce = transform.position - averageContact;
+            playerRigidbody.AddForce(repulsiveForce * 10.0F, ForceMode.Impulse);
+            Debug.Log("Collided");
+        }
+    }
+}
