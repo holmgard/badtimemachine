3977b61ba480af75a0ae82ffa77a494961fdd2fa
Artjoms-Jakovenko <artjoms.jakovenko@gmail.com>
2020-02-01 11:08:19 +0100

    Merged master

--- Assets/BadTimeMachine.cs
+++ Assets/BadTimeMachine.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public struct TimeStep
 {
     public Vector3 position;
     public Quaternion rotation;
-    
-    public TimeStep(Vector3 pos, Quaternion rot)
+    public float drag;
+    public float angDrag;
+    public Vector3 velocity;
+    public Vector3 angVelocity;
+
+    public TimeStep(Vector3 position, Quaternion rotation, float drag, float angDrag, Vector3 velocity, Vector3 angVelocity)
     {
-        position = pos;
-        rotation = rot;
+        this.position = position;
+        this.rotation = rotation;
+        this.drag = drag;
+        this.angDrag = angDrag;
+        this.velocity = velocity;
+        this.angVelocity = angVelocity;
     }
 }
 
@@ -21,36 +30,29 @@ public class BadTimeMachine : MonoBehaviour
     public bool backwardsInTime;
 
     bool recordCube = true;
+    Rigidbody myRigidbody;
 
     public List<TimeStep> timeLine;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         timeLine = new List<TimeStep>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        /*if (Input.GetKey(KeyCode.R))
-        {
-            forwardsInTime = false;
-            backwardsInTime = true;
-            //Debug.Log("Backwards in time!");
-        }
-        else
-        {
-            forwardsInTime = true;
-            backwardsInTime = false;
-        }*/
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
         if (recordCube)
         {
-            timeLine.Add((new TimeStep( gameObject.transform.position, gameObject.transform.rotation )));
+            timeLine.Add((new TimeStep(
+                transform.position,
+                transform.rotation,
+                myRigidbody.drag,
+                myRigidbody.angularDrag,
+                myRigidbody.velocity,
+                myRigidbody.angularVelocity
+            )));
         }
     }
 
@@ -62,7 +64,12 @@ public class BadTimeMachine : MonoBehaviour
             var lastStep = timeLine[timeLine.Count - 1];
             transform.position = lastStep.position;
             transform.rotation = lastStep.rotation;
+
+            myRigidbody.drag = lastStep.drag;
+            myRigidbody.angularDrag = lastStep.angDrag;
+            myRigidbody.velocity = lastStep.velocity;
+            myRigidbody.angularVelocity = lastStep.angVelocity;
             timeLine.RemoveAt(timeLine.Count - 1);
-        } 
+        }
     }
 }
